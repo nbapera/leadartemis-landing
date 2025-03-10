a9e2b48b7822162315906746c6b320b4b2cb5008
nbapera <valvejegovno@gmail.com>
2025-03-10 09:13:35 -0700

    aa

---
+++ data/footerLinks.ts
@@ -0,0 +1,6 @@
+export const footerLinks = [
+    {
+        href: "/",
+        label: "Home",
+    },
+];
---
+++ data/pricing.ts
@@ -0,0 +1,17 @@
+export const pricing = [
+    {
+        title: "Normal",
+        price: "$69.99",
+        features: ["Dashboard, including all analytics features", "Generating Leads Feature", "Filter phone from Leads"],
+        description: "This is a basic plan for beginners.",
+        fancy: false,
+    },
+    {
+        title: "Pro",
+        price: "$89.99",
+        features: ["Dashboard, including all analytics features", "Generating Leads Feature", "Filter website from Leads", "Filter phone from Leads", "Scrape emails and socials"],
+        description: "This is a pro plan for professionals.",
+        fancy: true,
+    },
+
+];
